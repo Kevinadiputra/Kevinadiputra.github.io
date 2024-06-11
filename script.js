0d4936efadc07749f703d0d973a0cf2f25aeff6c
kevinadiputra66 <kevinadiputra66@gmail.com>
2024-06-11 07:28:55 +0000

    commit

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// Toggle class active
+// Toggle class active humberger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika huberguer menu di klik
-document.querySelector("#humburger-menu").onclick = () => {
+document.querySelector("#humburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-//Klik di luar sidebar untuk menghilangkan nav
-const humburger = document.querySelector("#humburger-menu");
+// toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//Klik di luar elemen 
+const hm = document.querySelector("#humburger-menu");
+const sb = document.querySelector("#search-button")
 
 document.addEventListener("click", function (e) {
-  if (!humburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
+
+document.addEventListener("click", function (e) {
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
+
